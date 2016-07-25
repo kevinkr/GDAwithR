f1bed191802d75a70953ecdac31013063fa1c8a8
kevinkr <kevinkr@yahoo.com>
2016-07-25 10:45:29 -0500

    Updated to include ex. 6

--- Chapter1.R
+++ Chapter1.R
@@ -5,10 +5,10 @@ library(dplyr)
 library(GGally)
 library(vcd)
 library(extracat)
+library('RCurl')
+library('foreign')
 
 #http://www.gradaanwr.net/content/01-setting-the-scene/
-library(ggplot2)
-library(ggthemes)
 data(SpeedSki, package = "GDAdata")
 ggplot(SpeedSki, aes(x=Speed, fill=Sex)) + xlim(160, 220) +
   geom_histogram(binwidth=2.5) + xlab("Speed (km/hr)") +
@@ -82,7 +82,29 @@ ggplot(Pima.tr2, aes(type)) + geom_bar()
 #Exercise 3
 ggplot(Pima.tr2, aes(age,npreg)) + geom_point()
 
-#Ex 4
+#Exercise 4
+#Estimating the speed of light
+#There are 100 estimates of the speed of light made by Michelson in 1879, composed
+#of 5 groups of 20 experiments each (dataset michelson in the MASS package).
+#(a) What plot would you draw for showing the distribution of all the values
+#together? What conclusions would you draw?
+
+#plot  Experiments
+plot(michelson$Expt, michelson$Speed, xlab="Experiment number", main="Speed of light data")
+ggplot(data=michelson, aes(Expt, Speed)) + geom_point()
+
+#plot runs
+plot(michelson$Run, michelson$Speed, xlab="Run no.", main="Speed of light data")
+
+
+fm <- aov(michelson$Speed ~ michelson$Run + michelson$Expt)		# Run and Expt are factors
+summary(fm)
+#Result shows that Run is not an important factor.
+fm0 <- update( fm, . ~ . - michelson$Run)
+anova(fm0, fm)
+
+#(b) What plots might be useful for comparing the estimates from the 5 dif
+
 sol <- data.frame(michelson, package = "MASS")
 ggplot(michelson, aes(Speed)) + geom_histogram()
 ggplot(michelson, aes(Speed,Run)) + geom_point()
@@ -90,3 +112,120 @@ ggplot(michelson, aes(Speed,Run)) + geom_point()
 ggplot(michelson, aes(Speed, Run, color=Expt)) +
   geom_point() + theme(legend.position="bottom") +
   scale_colour_colorblind()
+
+with(michelson, boxplot(Speed ~ Expt)) 
+
+# I can add colour and labels. I can also save the results to an object.
+
+michelson.bp = with(michelson, boxplot(Speed ~ Expt, xlab="Experiment", las=1, 
+                                       ylab="Speed of Light - 299,000 m/s", 
+                                       main="Michelson-Morley Experiments",
+                                       col="slateblue1")) 
+# The current estimate of the speed of light, on this scale, is 734.5
+# Add a horizontal line to highlight this value.
+abline(h=734.5, lwd=2,col="purple")  #Add modern speed of light
+
+
+#Exercise 5
+#The liner Titanic sank on its maiden voyage in 1912 with great loss of life. The
+#dataset is provided in R as a table. Convert this table into a data frame using
+#data.frame(Titanic).
+data(Titanic)
+titanic <- as.data.frame(Titanic)
+
+require(graphics)
+mosaicplot(Titanic, main = "Survival on the Titanic")
+
+ggplot(data=titanic) + geom_bar(aes(x=Pclass))
+ggplot(data=titanic) + geom_bar(aes(x=Sex))
+
+#Read in full train data from titanic
+titanic <- read.csv(text=getURL("https://raw.githubusercontent.com/kevinkr/kaggle-titanic/master/Data/train.csv"),header = TRUE, stringsAsFactors = FALSE)
+
+qplot(Age, data=titanic, colour=as.factor(Pclass))
+qplot(Age, Fare, data=titanic, colour=as.factor(Pclass))
+
+qplot(Age, Fare, data=titanic, colour=as.factor(Pclass), facets=~Sex+Embarked)
+qplot(Age, Fare, data=titanic, colour=as.factor(Pclass), facets=Sex~Embarked)
+
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_point()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_hex()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_smooth()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_line()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_density2d()
+
+
+g <- ggplot(titanic, aes(Age, Fare))
+g + geom_hex() + geom_smooth()
+g + geom_point() + geom_smooth()
+g + geom_hex(alpha=0.3) + geom_smooth(color="red", lwd=2) + geom_point(size=3) 
+
+g <- ggplot(titanic, aes(Age))
+g + geom_histogram()
+g + geom_histogram(aes(fill=Sex))
+g + geom_density()
+g + geom_density(aes(fill=Sex))
+g + geom_density(aes(fill=Sex), alpha=0.3)
+
+ggplot(titanic, aes(Sex, Age)) + geom_boxplot()
+ggplot(titanic, aes(as.factor(Pclass), fill=Sex))+geom_bar()
+ggplot(titanic, aes(as.factor(Pclass), fill=as.factor(Survived)))+geom_bar()
+
+titanic <- titanic %>% mutate(Pclass.factor = as.factor(Pclass), Survived.factor = as.factor(Survived), age.group = cut(Age, breaks=seq(0,90,10)) ) 
+summary(titanic)
+
+#(a) What plot would you draw for showing the distribution of all the values
+#together? What conclusions would you draw?
+#(b) Draw a graphic to show the number sailing in each class. What order of
+#variable categories did you choose and why? Are you surprised by the different
+#class sizes?
+barchart <- ggplot(titanic, aes(as.factor(Pclass), fill=as.factor(Survived)))+geom_bar()
+barchart+xlab("Passenger Class")+ylab("Number of Passengers")+ggtitle("Survival by Passenger Class and Gender")+scale_fill_discrete(name = "", labels = c("Died", "Survived"))
+
+#(c) Draw graphics for the other three categorical variables. How good do you
+#think these data are? Why are there not more detailed data on the ages of
+#those sailing? Even if the age variable information (young and old) was
+#accurate, is this variable likely to be very useful in any modelling?
+
+
+#Exercise 6
+#Swiss
+#The dataset swiss contains a standardized fertility measure and various socioeconomic
+#indicators for each of 47 French-speaking provinces of Switzerland in
+#about 1888.
+#(a) What plot would you draw for showing the distribution of all the values
+#together? What conclusions would you draw?
+swiss.df <- data.frame(swiss)
+summary(swiss.df)
+plot(swiss.df)
+
+
+
+#(b) Draw graphics for each variable. What can you conclude from the distributions
+#concerning their form and possible outliers?
+
+h1 <- ggplot(swiss.df, aes(Fertility)) + geom_histogram()
+h2 <- ggplot(swiss.df, aes(Agriculture)) + geom_histogram()
+h3 <- ggplot(swiss.df, aes(Examination)) + geom_histogram()
+h4 <- ggplot(swiss.df, aes(Education)) + geom_histogram()
+h5 <- ggplot(swiss.df, aes(Catholic)) + geom_histogram()
+h6 <- ggplot(swiss.df, aes(Infant.Mortality)) + geom_histogram()
+grid.arrange(h1, h2, h3, h4, h5, h6, nrow=2)
+
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_point()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_hex()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_smooth()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_line()
+ggplot(titanic, aes(x=Age, y=Fare)) + geom_density2d()
+#(c) Draw a scatterplot of Fertility against % Catholic. Which kind of
+#areas have the lowest fertility rates?
+with(swiss.df, {
+  plot(Examination ~ Education)
+  title("Examination vs. Education")
+  plot(Catholic ~ Fertility)
+  title("% Catholic vs. Fertility")
+})
+
+#(d) What sort of relationship is there between the variables Education and
+#Agriculture?
+
